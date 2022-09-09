db898ce93ab955fb977854ec18c1082760a08c1a
Alex Solohub <le03grande@gmail.com>
2022-09-10 02:14:06 +0300

    html creating complete

---
+++ index.js
@@ -0,0 +1,9 @@
+const todoList = document.querySelector('.todo-list')
+const todoInput = document.querySelector('.input-block__input')
+const todoSubmit = document.querySelector('.input-block__submit')
+
+todoSubmit.addEventListener('click', submitHandler)
+
+function submitHandler(e) {
+   e.preventDefault
+}
